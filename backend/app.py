4f551841ef3ab115926e9fc55bd391994cc3ef4f
wen22-erh <joyfive123@gmail.com>
2025-06-30 12:39:36 +0800

    add connect mongodb function to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request
+from pymongo import MongoClient
 import csv
 import os
 app = Flask(__name__)
 
+client=MongoClient('mongodb://Localhost:27017/')
+db=client['fake-news-detector']
+collection=db['whitelist_urls']
+
 @app.route("/save_url", methods=["POST"])
 def save_url():
     data = request.get_json()
+    
+    collection.insert_one({"url":data["url"]})
+    
     with open("url_log.csv",'w',newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow([data["url"]])
-    return {"status": "url saved"}
+    return {"status": "url saved to MOngodb to csv"}
 
 # @app.route("/save_content", methods=["POST"])
 # def save_content():
