ead001b09711f1b545b366bee7ca08ab9ef7a033
wen22-erh <joyfive123@gmail.com>
2025-07-10 18:30:04 +0800

    add a box that can show url inside the box and the extension can get all the urls in the website

--- extension/content.js
+++ extension/content.js
@@ -3,3 +3,19 @@ chrome.runtime.sendMessage({
     action: "updateText",
     text: text,
 });
+let box = document.createElement("div");
+box.id = "box";
+box.textContent = "content";
+document.body.appendChild(box);
+document.addEventListener("mousemove", function (event) {
+    box.style.left = event.clientX + 10 + "px";
+    box.style.top = event.clientY + 10 + "px";
+    const target = event.target;
+    let url = null;
+    if (target.href) {
+        url = target.href;
+        box.textContent = url;
+    } else {
+        box.textContent = "";
+    }
+});
